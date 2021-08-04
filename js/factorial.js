581ce067595b4c4ed88b17f9318b1e77787f9402
saifur550 <isaifur550@icloud.com>
2021-08-04 19:15:10 +0800

    factorial while loop

--- js/factorial.js
+++ js/factorial.js
@@ -1,12 +1,64 @@
 
 
-// find factorial 
-var factorial = 1;
+// // find factorial 
+// var factorial = 1;
 
-for (i = 1; i <=7; i ++ ){
-    var outPut = (i);
-    console.log(outPut);
+// for (i = 1; i <=7; i ++ ){
+//     var outPut = (i);
+//     console.log(outPut);
    
-    factorial = factorial * i;
-    console.log(factorial);
+//     factorial = factorial * i;
+//     console.log(factorial);
+// }
+
+
+// // find factorial 
+// var factorial = 1;
+
+// for (i = 1; i <=10; i ++ ){
+//     var outPut = (i);
+//     console.log(outPut);
+   
+//     factorial = factorial * i;
+//     console.log(factorial);
+// }
+
+
+
+
+// function factorial (number){
+//     let fact = 1;
+//     for (i = 1; i <= number ; i ++ ){
+//         fact = fact * i;
+//     }
+//         return fact;
+    
+// }
+
+
+
+// // var firstNumber = factorial(7);
+// var firstNumber = prompt("enter the number ")
+// var firstOut = factorial(firstNumber);
+// console.log(firstOut);
+
+
+// var firstNumber2 = factorial(10);
+// console.log(firstNumber2);
+
+
+
+
+
+function getFactorial (passNumber){
+    let fact = 1;
+    for (i = 1; i <= passNumber; i ++ ){
+        fact =fact * i;
+    } return fact ;
+
 }
+
+
+var userInPut =  prompt("enter the number ");
+var finalOut = getFactorial(userInPut);
+console.log(finalOut);
---
+++ js/while.js
@@ -0,0 +1,14 @@
+function getFactorial (number){
+    let factorial = 1;
+    let i = 1;
+
+
+    while (i <number ){
+        factorial = factorial * 1;
+        return factorial;
+    }
+}
+
+
+const myFactorial = getFactorial(6)
+console.log(myFactorial);
